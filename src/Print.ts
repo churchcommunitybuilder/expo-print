d991051615feb0bd5922e2d78dae830dadb53bee
Doug Koehn <dkoehn@churchcommunitybuilder.com>
2020-04-23 12:33:09 -0600

    dk - Update to expo-print 8.1.0

--- src/Print.ts
+++ src/Print.ts
@@ -1,5 +1,5 @@
-import { Platform } from 'react-native';
 import { UnavailabilityError } from '@unimodules/core';
+import { Platform } from 'react-native';
 
 import ExponentPrint from './ExponentPrint';
 import {
@@ -18,7 +18,7 @@ export async function printAsync(options: PrintOptions): Promise<void> {
   if (Platform.OS === 'web') {
     return await ExponentPrint.print(options);
   }
-  if (!options.uri && !options.html && (Platform.OS === 'ios' && !options.markupFormatterIOS)) {
+  if (!options.uri && !options.html && Platform.OS === 'ios' && !options.markupFormatterIOS) {
     throw new Error('Must provide either `html` or `uri` to print');
   }
   if (options.uri && options.html) {
